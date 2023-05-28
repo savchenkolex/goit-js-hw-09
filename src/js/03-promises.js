160f7aabf6ede3f9932106aab1aa7473a10ef9b3
lex <savchenko.lex@gmail.com>
2023-05-28 15:17:12 +0300

    promise done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) =>{
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay);
+})
+}
+
+const formEl = document.querySelector(".form");
+
+formEl.addEventListener('submit', submitHandler);
+
+function submitHandler(event){
+  event.preventDefault();
+  const el = event.target.elements;
+
+  const amount = el.amount.valueAsNumber;
+  const delay = el.delay.valueAsNumber;
+  const step = el.step.valueAsNumber;
+
+  let setTime = delay;
+  for (let i = 1 ; i <= amount; i += 1 ){
+    if (i > 1 ){
+      setTime += step;
+    }
+    createPromise(i, setTime)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
